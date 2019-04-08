0023bbfe2ceed4a7ec50282a1ae2bd23d9308b15
Ruben Diaz <ruben.diaz@metrica.es>
2019-04-08 13:00:06 +0200

    Dialog can only be closed with button

--- Baraja/src/app/app.component.ts
+++ Baraja/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Baraja';
+
+  private success = new Subject<string>();
+
+  successMessage: string;
+
+  dlcPrice: number;
+
+  ngOnInit(): void {
+    this.success.subscribe((message) => this.successMessage = message);
+    this.success.pipe(
+      debounceTime(1000 * 10)
+    ).subscribe(() => this.successMessage = null);
+  }
+
+  buyDLC() {
+
+    // this.success.next(`${new Date()} - Message successfully changed.`);
+    this.dlcPrice = Math.floor((Math.random() * 50) + 50 );
+    this.success.next(this.dlcPrice + '' );
+  }
 }
