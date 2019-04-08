a498f1dc6d4153234fe009e457e838f7b8a62dca
Ruben Diaz <ruben.diaz@metrica.es>
2019-04-08 14:59:24 +0200

    Auto stash before merge of "master" and "origin/master"

--- BarajaAPI/src/main/java/controller/BarajaController.java
+++ BarajaAPI/src/main/java/controller/BarajaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import pojos.Baraja;
 import pojos.BarajaRepository;
 
+@CrossOrigin
 @RestController
 public class BarajaController {
 
--- BarajaAPI/src/main/java/metrica/Application.java
+++ BarajaAPI/src/main/java/metrica/Application.java
@@ -2,9 +2,15 @@ package metrica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@ComponentScan({ "controller" })
+@EntityScan("pojos")
+@EnableJpaRepositories("pojos")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
