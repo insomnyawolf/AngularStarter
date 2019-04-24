5f06d4f364f5a433928d7e1b9d23351656d5bb7b
Goyo <goyomet@gmail.com>
2019-04-24 10:36:04 +0200

    Revert "Seguridad"
    This reverts commit dc4ba246dec67ac6da97e283d1e3b7f3f55f83cb.

--- BarajaAPI/src/main/java/metrica/Application.java
+++ BarajaAPI/src/main/java/metrica/Application.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@ComponentScan({ "controller", "security" })
+@ComponentScan({ "controller" })
 @EntityScan("pojos")
 @EnableJpaRepositories("pojos")
 public class Application {
