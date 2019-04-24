599a8a4247ae0b5dab5c45327769931cadf369a4
Ruben Diaz <ruben.diaz@metrica.es>
2019-04-24 13:04:18 +0200

    Allow cross origin requests

--- BarajaAPI/src/main/java/security/WebSecurityConfig.java
+++ BarajaAPI/src/main/java/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -14,17 +19,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String ENCODED_PASSWORD = "$2a$10$AIUufK8g6EFhBcumRRV2L.AQNz3Bjp7oDQVFiO5JJMBFZQ6x2/R/2"; //<-- contraseña "secret123" codificada con BCrypt
 	
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	 http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
-	   .loginPage("/login").permitAll().and().logout().permitAll();
+		http.cors().and()
+		.csrf().disable()
+		.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and()
+		.formLogin().loginPage("/login").permitAll().and()
+		.logout();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.applyPermitDefaultValues();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 	
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	 auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("user").password(ENCODED_PASSWORD)
-	   .roles("USER");
+	protected void configure(AuthenticationManagerBuilder authManager) throws Exception {
+		authManager.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("user").password(ENCODED_PASSWORD).roles("USER");
+	 
+	 	
 	}
 	
 	@Bean
